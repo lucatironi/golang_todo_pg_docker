da426a6be5581cf9534a493260c97ba44b1e9a46
Luca Tironi <luca.tironi@gmail.com>
2016-07-09 13:10:22 +0200

    Use better linking between app and db

--- api/db.go
+++ api/db.go
@@ -9,10 +9,7 @@ import (
 
 func InitDB() *pg.DB {
   db := pg.Connect(&pg.Options{
-    Addr: fmt.Sprintf("%v:%v",
-      os.Getenv("DOCKER_POSTGRES_1_PORT_5432_TCP_ADDR"),
-      os.Getenv("DOCKER_POSTGRES_1_PORT_5432_TCP_PORT"),
-    ),
+    Addr: fmt.Sprintf("%v:%v", os.Getenv("DB_ADDR"), os.Getenv("DB_PORT")),
     User: os.Getenv("DB_USER"),
     Password: os.Getenv("DB_PSWD"),
     Database: os.Getenv("DB_NAME"),
