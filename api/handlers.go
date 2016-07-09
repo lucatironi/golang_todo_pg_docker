0009eaef283598658df756d9b808cdba01ba929a
Luca Tironi <luca.tironi@gmail.com>
2016-07-09 15:15:45 +0200

    Remove unused functions

--- api/handlers.go
+++ api/handlers.go
@@ -11,18 +11,6 @@ import (
   "github.com/gorilla/mux"
 )
 
-func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
-  w.Header().Set("Content-Type", "application/json; charset=utf-8")
-  w.WriteHeader(code)
-  fmt.Fprintf(w, "{message: %q}", message)
-}
-
-func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
-  w.Header().Set("Content-Type", "application/json; charset=utf-8")
-  w.WriteHeader(code)
-  w.Write(json)
-}
-
 func Index(w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "Welcome\n")
 }
