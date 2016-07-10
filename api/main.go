2e756dd667d7203678dd24de56f8bec36eb8f975
Luca Tironi <luca.tironi@gmail.com>
2016-07-10 16:31:40 +0200

    Change boot message's grammar

--- api/main.go
+++ api/main.go
@@ -12,7 +12,7 @@ var db = InitDB()
 func main() {
   router := NewRouter()
 
-  log.Printf(fmt.Sprintf("Service listening at port %v", os.Getenv("PORT")))
+  log.Printf(fmt.Sprintf("Service listening on port %v", os.Getenv("PORT")))
 
   log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", os.Getenv("PORT")), router))
 }
